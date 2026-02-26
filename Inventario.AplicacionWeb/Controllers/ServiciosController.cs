940b8914eacbf5408f12c0077c21f0766057202c
Oscar <oscarpandilo@gmail.com>
2026-02-26 17:41:26 -0600

    vistas de tablarequiservicios y formulariorequiservicios maquetadas

--- Inventario.AplicacionWeb/Controllers/ServiciosController.cs
+++ Inventario.AplicacionWeb/Controllers/ServiciosController.cs
@@ -1,9 +1,22 @@
+using AutoMapper;
+using Inventario.AplicacionWeb.Models.ViewModels;
+using Inventario.BLL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Inventario.AplicacionWeb.Controllers
 {
     public class ServiciosController : Controller
     {
+        private readonly IUsuarioService _usuarioService;
+        private readonly IMapper _mapper;
+
+        public ServiciosController(IUsuarioService usuarioService, IMapper mapper)
+        {
+            _usuarioService = usuarioService;
+            _mapper = mapper;
+        }
+
         [HttpGet]
         public IActionResult TablaRequisicionServicios()
         {
@@ -12,10 +25,18 @@ namespace Inventario.AplicacionWeb.Controllers
         }
 
         [HttpGet]
-        public IActionResult FormularioRequisicionServicios()
+        public async Task<IActionResult> FormularioRequisicionServicios()
         {
+            var idClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(idClaim) || !int.TryParse(idClaim, out int idUsuario))
+                return RedirectToAction("Login", "Acceso");
+
             ViewData["Title"] = "FormularioRequisicionServicios";
-            return View();
+
+            var dto = await _usuarioService.ObtenerDatosDepartamento(idUsuario);
+            var vm = _mapper.Map<VMRequiForm>(dto);
+
+            return View(vm);
         }
     }
 }
